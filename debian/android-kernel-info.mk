a9fd31c58ae3aa8081a17fb06b14f909b5d3cf58
Eugenio Paolantonio (g7) <me@medesimo.eu>
2024-01-09 00:43:18 +0100

    [linux-android-common-5.10-android12] Initial content for 5.10-android12 GKI
    Signed-off-by: Eugenio Paolantonio (g7) <me@medesimo.eu>

---
+++ debian/android-kernel-info.mk
@@ -0,0 +1,9 @@
+# Do not modify! These values have been extracted from the current
+# android common kernel sources. See ../update_submodule.sh
+
+# SPDX-License-Identifier: GPL-2.0
+VERSION = 5
+PATCHLEVEL = 10
+SUBLEVEL = 198
+EXTRAVERSION =
+NAME = Dare mighty things
---
+++ update_submodule.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Updates common kernel submodule without fetching it
+#
+
+set -e
+
+usage() {
+	echo "USAGE ${0} <commit-sha>"
+	exit 1
+}
+
+[ -n "${1}" ] || usage
+
+MAKEFILE="https://raw.githubusercontent.com/droidian-devices/android_kernel_common/${1}/Makefile"
+
+if ! curl -L --silent --head --fail "${MAKEFILE}" &>/dev/null; then
+	echo "${1}: commit not found" >&2
+	exit 1
+fi
+
+git update-index --add --cacheinfo 160000,${1},android_kernel_common
+
+# Fetch kernel info
+cat > debian/android-kernel-info.mk <<EOF
+# Do not modify! These values have been extracted from the current
+# android common kernel sources. See ../update_submodule.sh
+
+EOF
+# head -n 6 is rather ugly, maybe replace with something more clean in future
+curl -L "${MAKEFILE}" 2>/dev/null | head -n 6 >> debian/android-kernel-info.mk
+
+echo "Please check the changes, rebuild debian/control and commit"
